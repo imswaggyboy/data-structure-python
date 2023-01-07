00af318c2c9d3d06a2242ba50f40f906e5c09a6f
Surya Murugan <75663923+imswaggyboy@users.noreply.github.com>
2023-01-07 14:18:08 +0530

    Update quick_sort_recursion.py

--- recursion/quick_sort_recursion.py
+++ recursion/quick_sort_recursion.py
@@ -3,9 +3,13 @@ def qsort(l):
         return qsort([x for x in l[1:] if x < l[0]]) + l[:1] + qsort([x for x in l[1:] if x >= l[0]])
     return []
 
+
 lst = [1,4,2,5,3,6,3,7,4,0]
+#passing list in qsort func
 qsort(lst)
+#output
 [0, 1, 2, 3, 3, 4, 4, 5, 6, 7]
+
 type(lst[:1])
 <class 'list'>
 lst[0]
